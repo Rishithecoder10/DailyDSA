21f36b477cf3399fcce386bcf3030f96152791ed
Rishi Patel <150878128+Rishithecoder10@users.noreply.github.com>
2025-01-25 10:42:04 +0530

    Create Stack_Permutations.cpp

---
+++ Stacks & Queues/Stack_Permutations.cpp
@@ -0,0 +1,12 @@
+ int isStackPermutation(int N,vector<int> &A,vector<int> &B){
+        stack<int> st;
+        int j=0;
+        for(int i=0;i<N;i++){
+            st.push(A[i]);
+            while(!st.empty()&&st.top()==B[j]){
+                st.pop();
+                j++;
+            }
+        }
+        return st.empty()?1:0;
+    }
