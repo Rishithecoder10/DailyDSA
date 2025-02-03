455b203d522c4f00df6f31c56e47b94fe19b5df7
Rishi Patel <150878128+Rishithecoder10@users.noreply.github.com>
2025-02-03 19:23:32 +0530

    Create Longest_Increasing_Subsequence_brute_force.cpp

---
+++ cp/Longest_Increasing_Subsequence_brute_force.cpp
@@ -0,0 +1,39 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    
+    vector<int> dp(n, 1);  
+    
+   
+    for(int i = 1; i < n; i++) {
+        
+        for(int j = 0; j < i; j++) {
+            
+            if(arr[i] > arr[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+    
+    
+    int maxLen = 1;
+    for(int i = 0; i < n; i++) {
+        maxLen = max(maxLen, dp[i]);
+    }
+    
+    cout << maxLen;
+    return 0;
+}
