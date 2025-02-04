a549d30373436260d3348694c37840dbf26a30fc
Rishi Patel <150878128+Rishithecoder10@users.noreply.github.com>
2025-02-04 17:30:18 +0530

    Create Height_of_Binary_Tree_using_recusion.cpp

---
+++ binary_tree/Height_of_Binary_Tree_using_recusion.cpp
@@ -0,0 +1,8 @@
+ int height(Node* node) {
+        // code here
+        if(node==NULL){
+            return -1;
+        }
+        return 1+(max(height(node->left),height(node->right)));
+        
+    }
